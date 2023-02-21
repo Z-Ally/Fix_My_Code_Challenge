557c6e08958368ef8c25af8e7b1bb9fea3c5fa6a
Z-Ally <12zaheeraally@gmail.com>
2023-02-21 09:08:13 -0800

    2

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -11,16 +11,12 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-    
+
     # insert result at the right position
     is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
+    result.each_with_index do |val, i|
+        if val > i_arg
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
