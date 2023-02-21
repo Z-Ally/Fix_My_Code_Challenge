20a43e8f2cad2c1145cfcc8f85a8a3b947cd7a83
Z-Ally <12zaheeraally@gmail.com>
2023-02-21 09:17:10 -0800

    3

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,51 +54,35 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
     print("Test User")
 
     user_1 = User()
-    if user_1.id is None:
-        print("New User should have an id")
+    assert user_1.id is not None, "New User should have an id"
 
     user_2 = User()
-    if user_1.id == user_2.id:
-        print("User.id should be unique")
+    assert user_1.id != user_2.id, "User.id should be unique"
 
     u_pwd = "myPassword"
     user_1.password = u_pwd
-    if user_1.password == u_pwd:
-        print("User.password should be hashed")
+    assert user_1.password != u_pwd, "User.password should be hashed"
 
-    if user_2.password is not None:
-        print("User.password should be None by default")
+    assert user_2.password is None, "User.password should be None by default"
 
     user_2.password = None
-    if user_2.password is not None:
-        print("User.password should be None if setter to None")
+    assert user_2.password is None, "User.password should be None if set to None"
 
     user_2.password = 89
-    if user_2.password is not None:
-        print("User.password should be None if setter to an integer")
+    assert user_2.password is None, "User.password should be None if set to an integer"
 
-    if user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right \
-password")
+    assert user_1.is_valid_password(u_pwd), "is_valid_password should return True if it's the right password"
+    assert not user_1.is_valid_password("Fakepwd"), "is_valid_password should return False if it's not the right password"
+    assert not user_1.is_valid_password(None), "is_valid_password should return False if compared with None"
+    assert not user_1.is_valid_password(89), "is_valid_password should return False if compared with an integer"
+    assert not user_2.is_valid_password("No pwd"), "is_valid_password should return False if no password set before"
 
-    if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right \
-password")
-
-    if user_1.is_valid_password(None):
-        print("is_valid_password should return False if compare with None")
-
-    if user_1.is_valid_password(89):
-        print("is_valid_password should return False if compare with integer")
-
-    if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no password set \
-before")
+    print("All tests passed")
 
